b045d2a826b57f8c6d95c21fbc88b87b4818c3a2
willfull <willfull.git.box@yandex.ua>
2020-08-23 23:41:58 +0300

    Add - Webpack split config for client and server

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,39 @@
-const path = require('path');
+const path      = require('path');
+const nodeExtrn = require('webpack-node-externals');
+
+const moduleObj = {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loaders: ["babel-loader"]
+    }
+  ]
+}
 
 const client = {
   entry: {
-    'client': path.join(__dirname, '/src/client/index.js')
+    client: path.join(__dirname, '/src/client/index.js')
   },
   target: 'web',
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist/public')
-  }
+  },
+  module: moduleObj
 }
 
 const server = {
   entry: {
-    'server': path.join(__dirname, '/src/server/index.js')
+    server: path.join(__dirname, '/src/server/index.js')
   },
   target: 'node',
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: moduleObj,
+  externals: [nodeExtrn()]
 }
 
 module.exports = [client, server];

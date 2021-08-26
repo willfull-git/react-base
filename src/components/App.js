530e9037f5ec63e464d27b5818c71423aa5f80e7
willfull <willfull.box@yandex.ua>
2021-08-26 21:17:00 +0300

    Ref - Changed Class component template to Functional ones, + small refactoring

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React  from 'react';
+import styles from './app.css';
+
+export default ()=>{
+  return (
+    <div className={styles.container}>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React    from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './app.js';
+import App from './components/App';
 
 ReactDOM.render(
   <App/>,
